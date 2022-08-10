9f5f71578119f39f3ffba151051f07e4f23fdc8f
mimori <kazumasa071210@gmail.com>
2022-08-10 10:37:27 +0900

    🎉 スタート

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import { Button } from "@mantine/core";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Button variant="light" size="xl">
+        test
+      </Button>
+    </>
+  );
+};
+
+export default Home;
