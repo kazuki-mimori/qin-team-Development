8ce0002cd2a576a28a632e5965a89ab9800f0656
mimori <kazumasa071210@gmail.com>
2022-08-17 22:32:37 +0900

    Githubエリアを完成

--- pages/component/Github.tsx
+++ pages/component/Github.tsx
@@ -10,6 +10,39 @@ import {
 import { FaRegStar } from "react-icons/fa";
 import { TbGitFork } from "react-icons/tb";
 function Github() {
+  interface GithubContent {
+    title: string;
+    skl: string;
+    starIcon: number;
+    otherIcon: number;
+  }
+  const GithubContent = [
+    {
+      title: "lightsound/nexst-tailwind",
+      skl: "Next.js starter template.",
+      starIcon: 117,
+      otherIcon: 18,
+    },
+    {
+      title: "lightsound/nexst-tailwind",
+      skl: "Next.js starter template.",
+      starIcon: 117,
+      otherIcon: 18,
+    },
+    {
+      title: "lightsound/nexst-tailwind",
+      skl: "Next.js starter template.",
+      starIcon: 117,
+      otherIcon: 18,
+    },
+    {
+      title: "lightsound/nexst-tailwind",
+      skl: "Next.js starter template.",
+      starIcon: 117,
+      otherIcon: 18,
+    },
+  ];
+
   return (
     <>
       <div className="mt-px">
@@ -18,40 +51,43 @@ function Github() {
           <Space h="xl" />
           <div className="border-b-2 divide-gray-200 ..." />
           <div className="my-6">
-            <h3 className="text-lg">lightsound/nexst-tailwind</h3>
-            <p className="text-base font-extralight">
-              Next.js starter template.
-            </p>
-
-            <Group spacing="sm">
-              <ActionIcon size="lg">
-                <FaRegStar className="ml-1" />
-              </ActionIcon>
-              <p className="mr-10">117</p>
-              <ActionIcon size="lg">
-                <TbGitFork className="mr-1" />
-              </ActionIcon>
-              <p>18</p>
-            </Group>
+            {GithubContent.map((content: any) => {
+              return (
+                <>
+                  <h3 className="text-lg">{content.title}</h3>
+                  <p className="text-base font-extralight">{content.skl}</p>
+                  <Group spacing="sm">
+                    <ActionIcon size="lg">
+                      <FaRegStar className="ml-1" />
+                    </ActionIcon>
+                    <p className="mr-10">{content.starIcon}</p>
+                    <ActionIcon size="lg">
+                      <TbGitFork className="mr-1" />
+                    </ActionIcon>
+                    <p>{content.otherIcon}</p>
+                  </Group>
+                  <Progress
+                    sections={[
+                      { value: 65.5, color: "#3178C6" },
+                      { value: 33.7, color: "#F1E05A" },
+                      { value: 0.8, color: "#EDEDED" },
+                    ]}
+                  />
+                  <List className="flex mb-10">
+                    <Text size="sm" weight={700}>
+                      <List.Item>TypeScript</List.Item>
+                    </Text>
+                    <Text size="sm" weight={700}>
+                      <List.Item>JavaScript</List.Item>
+                    </Text>
+                    <Text size="sm" weight={700}>
+                      <List.Item>Other</List.Item>
+                    </Text>
+                  </List>
+                </>
+              );
+            })}
           </div>
-          <Progress
-            sections={[
-              { value: 65.5, color: "#3178C6" },
-              { value: 33.7, color: "#F1E05A" },
-              { value: 0.8, color: "#EDEDED" },
-            ]}
-          />
-          <List className="flex mb-2">
-            <Text size="sm" weight={700}>
-              <List.Item>TypeScript</List.Item>
-            </Text>
-            <Text size="sm" weight={700}>
-              <List.Item>JavaScript</List.Item>
-            </Text>
-            <Text size="sm" weight={700}>
-              <List.Item>Other</List.Item>
-            </Text>
-          </List>
 
           <Space h="xl" />
           <Group position="center">
